ee9609b3f1bb53fb9a9787bea20527dd94bc6cfc
SzczurekYT <kamilsecret8@gmail.com>
2023-08-26 15:58:19 +0200

    This can be removed too.

--- lib/src/global_data.dart
+++ lib/src/global_data.dart
@@ -9,12 +9,6 @@ import 'package:wave_mixer/src/graph/port.dart';
 const double portDotSize = 12;
 const Offset portDotOffset = Offset(portDotSize / 2, portDotSize / 2);
 
-final Lens<Port, Node> nodePortLens = Lens(
-  view: (store) => store.ports.values.first,
-  set: (store, value) =>
-      store.copyWith(ports: store.ports.add(value.id, value)),
-);
-
 final nodeGraphNotifierProvider =
     NotifierProvider<NodeGraphNotifier, IMap<String, Node>>(
   NodeGraphNotifier.new,
